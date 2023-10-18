29bbc8a550f03f2b6eaa79a844914134c3f81b2a
Akash <sgc.akash.22oct.11sci@gmail.com>
2023-10-18 20:55:05 +0530

    update lesson 9 completed

--- lesson1/src/main.jsx
+++ lesson1/src/main.jsx
@@ -9,9 +9,14 @@ const loc = document.getElementById("root");
 const root = ReactDOM.createRoot(loc);
 root.render(
   <>
-    <h1>
+    <h1 className="heading" contentEditable="true" spellCheck="false">
       Hello {name} {surname}
     </h1>
-    <p>Lucky number is {Math.floor(Math.random() * value) + 1}</p>
+    <div>
+      <img src="https://images.pexels.com/photos/5591708/pexels-photo-5591708.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"></img>
+      <img src="https://images.pexels.com/photos/194096/pexels-photo-194096.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"></img>
+      <img src="https://images.pexels.com/photos/3336993/pexels-photo-3336993.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"></img>
+      <img src="https://picsum.photos/200/300"></img>
+    </div>
   </>
 );
