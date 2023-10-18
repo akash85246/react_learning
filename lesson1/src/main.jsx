1286c683875397a45be65fc8098df1e40d411573
Akash <sgc.akash.22oct.11sci@gmail.com>
2023-10-18 15:27:43 +0530

    hello world

--- lesson1/src/main.jsx
+++ lesson1/src/main.jsx
@@ -1,10 +1,20 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+// import React from "react";
+// import ReactDOM from "react-dom/client";
+// import App from "./App.jsx";
+// import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+// ReactDOM.createRoot(document.getElementById("root")).render(
+//   <>
+//     <h1>Hello world</h1>
+//     <p>I am lost</p>
+//   </>
+// );
+
+//doubt
+var React = require("react");
+var ReactDOM = require("react-dom");
+
+const element = <h1>Hello World</h1>;
+const root = document.getElementById("root");
+
+ReactDOM.render(element, root);
