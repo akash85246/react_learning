41e23add0053577958674d268de2366d510a4795
Akash <sgc.akash.22oct.11sci@gmail.com>
2023-10-18 20:23:36 +0530

    update the watch

--- lesson1/src/main.jsx
+++ lesson1/src/main.jsx
@@ -1,17 +1,32 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
+import React, { useState, useEffect } from "react";
+import ReactDOM from "react-dom";
+function App() {
+  const [date, setDate] = useState(new Date());
+  const name = "Akash";
+  const surname = "Rajput";
 
-var name = "Akash";
-var surname = "Rajput";
-const value = 8;
+  setInterval(() => {
+    setDate(new Date());
+  }, 100);
+
+  const year = date.getFullYear();
+  const month = date.getMonth() + 1;
+  const day = date.getDate();
+  const hour = date.getHours();
+  const min = date.getMinutes();
+  const sec = date.getSeconds();
+  const milis = date.getMilliseconds();
+
+  return (
+    <div>
+      <h1>My Watch</h1>
+      <h2>{"Date " + day + " Month " + month + " Year " + year}</h2>
+      <h2>{hour + " : " + min + " : " + sec + " : " + milis}</h2>
+      <h3>Created by {name + " " + surname}</h3>
+    </div>
+  );
+}
 
 const loc = document.getElementById("root");
 const root = ReactDOM.createRoot(loc);
-root.render(
-  <>
-    <h1>
-      Hello {name} {surname}
-    </h1>
-    <p>Lucky number is {Math.floor(Math.random() * value) + 1}</p>
-  </>
-);
+root.render(<App />);
