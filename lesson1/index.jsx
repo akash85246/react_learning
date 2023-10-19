fa9bb92a133a5b868885a400858c8b32afdbd49e
Akash <sgc.akash.22oct.11sci@gmail.com>
2023-10-19 20:09:46 +0530

    updated emoji

---
+++ lesson1/index.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./src/App";
+import emojipedia from "./emojipedia";
+
+
+const container = document.getElementById("root");
+const root = ReactDOM.createRoot(container);
+root.render(<App />);
--- lesson1/src/App.jsx
+++ lesson1/src/App.jsx
@@ -1,43 +1,45 @@
-// import { useState } from "react";
-// import reactLogo from "./assets/react.svg";
-// import viteLogo from "/vite.svg";
-// import "./App.css";
-
-// function App() {
-//   const [count, setCount] = useState(0);
-
-//   return (
-//     <>
-//       <div>
-//         <a href="https://vitejs.dev" target="_blank">
-//           <img src={viteLogo} className="logo" alt="Vite logo" />
-//         </a>
-//         <a href="https://react.dev" target="_blank">
-//           <img src={reactLogo} className="logo react" alt="React logo" />
-//         </a>
-//       </div>
-//       <h1>Vite + React</h1>
-//       <div className="card">
-//         <button onClick={() => setCount((count) => count + 1)}>
-//           count is {count}
-//         </button>
-//         <p>
-//           Edit <code>src/App.jsx</code> and save to test HMR
-//         </p>
-//       </div>
-//       <p className="read-the-docs">
-//         Click on the Vite and React logos to learn more
-//       </p>
-//     </>
-//   );
-// }
-
-// export default App;
 import React from "react";
+import Card from "../tile";
+import emojipedia from "../emojipedia";
 
-function Heading() {
-  return <h1>my list</h1>;
+function create(emojic) {
+  return (
+    <Card
+      key={emojic.ID}
+      emoji={emojic.emoji}
+      name={emojic.name}
+      descript={emojic.meaning}
+    />
+  );
 }
 
+function App() {
+  return (
+    <div>
+      <h1>
+        <span>emojipedia</span>
+      </h1>
 
-export default Heading;
+      <dl className="dictionary">
+        {/* <Card 
+        emoji="🙏" 
+        name="Person With Folded Hands" 
+        descript="Two hands pressed together. Is currently very introverted, saying a prayer, or hoping for enlightenment. Is also used as a “high five” or to say thank you." />
+
+        <Card 
+          emoji="💪" 
+        name="Tense Biceps" 
+        descript="“You can do that!” or “I feel strong!” Arm with tense biceps. Also used in connection with doing sports, e.g. at the gym."
+        />
+        <Card 
+              emoji: "🤣", 
+        name="Rolling On The Floor, Laughing" 
+        descript="This is funny! A smiley face, rolling on the floor, laughing. The face is laughing boundlessly. The emoji version of “rofl“. Stands for „rolling on the floor, laughing“."
+        /> */}
+        {emojipedia.map(create)}
+      </dl>
+    </div>
+  );
+}
+
+export default App;
