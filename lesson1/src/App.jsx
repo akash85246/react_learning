0717b67140b9ebbff199b00a8ea524972d014017
Akash <sgc.akash.22oct.11sci@gmail.com>
2023-10-19 22:59:04 +0530

    update hooks

--- lesson1/src/App.jsx
+++ lesson1/src/App.jsx
@@ -1,12 +1,23 @@
 import React from "react";
-import Form from "./Form";
-
-var userIsRegistered = false;
+import { useState } from "react";
 
 function App() {
+  const [num, setNum] = useState(0);
+  const [num1, setNum1] = useState(0);
+
+  function inc() {
+    setNum(num + 1);
+  }
+  function dec() {
+    setNum1(num1 - 1);
+  }
+
   return (
     <div className="container">
-      <Form />
+      <h1>{num}</h1>
+      <button onClick={inc}>+</button>
+      <h1>{num1}</h1>
+      <button onClick={dec}>-</button>
     </div>
   );
 }
